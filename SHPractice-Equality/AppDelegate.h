246c95d767eb3dd676278d989d8e4f142e2948d2
wangxiang <wangxiang@yixia.com>
2018-11-14 14:37:28 +0800

    Initial Commit

---
+++ SHPractice-Equality/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SHPractice-Equality
+//
+//  Created by shine on 2018/11/14.
+//  Copyright © 2018 shine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SHPractice-Equality/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SHPractice-Equality
+//
+//  Created by shine on 2018/11/14.
+//  Copyright © 2018 shine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
